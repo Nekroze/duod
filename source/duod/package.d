a0e34570a4860c8434ecf0f69b38ab451343a0e7
Taylor "Nekroze" Lawson <tlawson@nekroze.com>
2015-02-14 10:22:43 +1000

    Layed out package structure

---
+++ source/duod/package.d
@@ -0,0 +1,11 @@
+/+++++++++++++++++++++++++++++++++++
+ + DuoD
+ + Duo asset management integration
+ +
+ + Authors: Taylor "Nekroze" Lawson, <tlawson@nekroze.com>
+ + License: MIT
+ + Copyright: 2015, Taylor Lawson
+ + History:
+ +      v0.0.0 - Fleshing out basic implementations and API.
+ +/
+module duod;
