f749a216a3a6a61639313d0b2ca3cebb24f85cb0
Taylor "Nekroze" Lawson <tlawson@nekroze.com>
2015-02-16 08:07:27 +1000

    Added documentation to duod.utilities

--- source/duod/utilities.d
+++ source/duod/utilities.d
@@ -1,29 +1,30 @@
+/++ Utilities for duod.
+ +  Provides tools for checking if the build commands are available and executing them.
+ +/
 module duod.utilities;
 import std.array : appender;
 import std.process : executeShell, pipeShell, Redirect, wait;
-
+/// True if the `duo` command is avilable at runtime
 static bool hasDuo;
+/// True if the `yuglify` command is avilable at runtime
 static bool hasYuglify;
-
+/// Once per runtime for all threads, checks for the existance of duo and yuglify commands.
 shared static this () {
     hasDuo = !executeShell("duo --version").status;
     hasYuglify = !executeShell("yuglify --version").status;
 }
-
-string getOutput (string command) {
-    auto pipes = pipeShell (command, Redirect.stdout);
-    scope (exit) wait (pipes.pid);
-
-    auto output = appender!string ();
-    foreach (line; pipes.stdout.byLine ())
-        output ~= line;
-    return output.data;
-}
-
-string getOutput (string command, string input) {
+/++ Get the output of a shell command after passing it some input.
+ +  Params:
+ +      command =   Shell command to run.
+ +      input   =   Input to pass to the command via stdin. Defaults to no input.
+ +  Returns: A string containing all stdout output from the given commands execution.
+ +/
+string getOutput (string command, string input="") {
     auto pipes = pipeShell (command, Redirect.stdin | Redirect.stdout);
     scope (exit) wait (pipes.pid);
-    pipes.stdin.rawWrite (input);
+
+    if (input)
+        pipes.stdin.rawWrite (input);
     pipes.stdin.close ();
 
     auto output = appender!string ();
